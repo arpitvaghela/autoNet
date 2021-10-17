d477e4b2a5a40bd6bd36f58842c9703e9196194b
Kaushal1011 <kaushalpatil10@gmail.com>
2021-10-17 14:22:31 +0530

    worker in container with gpu access and darts algorithm

--- autonet-kafka/worker-paaji/main.py
+++ autonet-kafka/worker-paaji/main.py
@@ -6,31 +6,40 @@ from aiokafka import AIOKafkaConsumer
 from core.config import KAFKA_INSTANCE
 from core.config import PROJECT_NAME
 from loguru import logger
+import torch
 
-topicname="geostream"
+topicname = "geostream"
+
 
 async def consume(consumer, topicname):
     async for msg in consumer:
         return msg.value.decode()
 
+
 async def func():
 
     loop = asyncio.get_event_loop()
     consumer = AIOKafkaConsumer(
-            topicname,
-            loop=loop,
-            client_id=PROJECT_NAME,
-            bootstrap_servers=KAFKA_INSTANCE,
-            enable_auto_commit=False,
-        )
+        topicname,
+        loop=loop,
+        client_id=PROJECT_NAME,
+        bootstrap_servers=KAFKA_INSTANCE,
+        enable_auto_commit=False,
+    )
 
     await consumer.start()
 
     while True:
         data = await consume(consumer, topicname)
-            # response = ConsumerResponse(topic=topicname, **json.loads(data))
+        # response = ConsumerResponse(topic=topicname, **json.loads(data))
         response = str(data)
+        if response:
+            print("GPU Available")
+            print(torch.cuda.is_available())
+
         print(f"worker:main[77]: {response}")
-        logger.info(response)
+        # logger.info(response)
+        print("HEllo")
+
 
 asyncio.run(func())
